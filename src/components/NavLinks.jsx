2cf77a2881a3d6d5866fbab3794e112619cfa7ec
Nikki Wong <nikkiwong@miami.edu>
2018-11-09 11:38:09 -0800

    style nav links

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,21 +1,44 @@
 import React from 'react';
 
 function NavLinks() {
+  const navLinksBackgroundStyles = {
+    backgroundColor: '#99073a'
+  }
+  const navLinksListStyles = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    listStyle: 'none',
+    color: 'white',
+    marginLeft: '20%',
+    marginRight: '20%',
+    marginTop: '0',
+    paddingTop: '10px',
+    paddingBottom: '10px'
+  }
+  const navLinksLinkStyles = {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: '14px',
+    textDecoration: 'none',
+    transition: 'all 0.3s cubic-bezier(0,0,.25,1)'
+  }
   return (
     <div>
-      <div>
+      <div style={navLinksBackgroundStyles}>
         <div>
-          <ul>
-            <li><a href="https://ohmy.disney.com/quiz/">QUIZ</a></li>
-            <li><a href="#">NEWS</a></li>
-            <li><a href="https://ohmy.disney.com/quotes/">QUOTES</a></li>
-            <li><a href="https://ohmy.disney.com/movies/">MOVIES</a></li>
-            <li><a href="https://ohmy.disney.com/food/">FOOD</a></li>
-            <li><a href="https://ohmy.disney.com/parks/">PARKS</a></li>
-            <li><a href="https://ohmy.disney.com/tv/">TV</a></li>
-            <li><a href="https://ohmy.disney.com/music/">MUSIC</a></li>
-            <li><a href="https://video.disney.com/oh-my-disney">VIDEO</a></li>
-            <li><a href="https://ohmy.disney.com/shop/">SHOP</a></li>
+          <ul style={navLinksListStyles}>
+            <li><a style={navLinksLinkStyles} href="https://ohmy.disney.com/quiz/">QUIZ</a></li>
+            <li><a style={navLinksLinkStyles} href="#">NEWS</a></li>
+            <li><a style={navLinksLinkStyles} href="https://ohmy.disney.com/quotes/">QUOTES</a></li>
+            <li><a style={navLinksLinkStyles} href="https://ohmy.disney.com/movies/">MOVIES</a></li>
+            <li><a style={navLinksLinkStyles} href="https://ohmy.disney.com/food/">FOOD</a></li>
+            <li><a style={navLinksLinkStyles} href="https://ohmy.disney.com/parks/">PARKS</a></li>
+            <li><a style={navLinksLinkStyles} href="https://ohmy.disney.com/tv/">TV</a></li>
+            <li><a style={navLinksLinkStyles} href="https://ohmy.disney.com/music/">MUSIC</a></li>
+            <li><a style={navLinksLinkStyles} href="https://video.disney.com/oh-my-disney">VIDEO</a></li>
+            <li><a style={navLinksLinkStyles} href="https://ohmy.disney.com/shop/">SHOP</a></li>
           </ul>
         </div>
       </div>
