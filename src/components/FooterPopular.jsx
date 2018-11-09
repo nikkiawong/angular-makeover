27d53714ff5b5f71d827dcb330fa06768b426898
Nikki Wong <nikkiwong@miami.edu>
2018-11-09 15:18:27 -0800

    add footer popular posts

--- src/components/FooterPopular.jsx
+++ src/components/FooterPopular.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import FooterPopularPost from './FooterPopularPost';
+
+const masterFooterPopularList = [
+  {
+    headline: "Can You Guess the ‘90s Disney Movie from One Random Clue?",
+    image: "https://ohmy.disney.com/wp-content/uploads/2018/09/ET_OhMyDisney_Guess90sMovieFromOneClue.jpg",
+    url: "https://ohmy.disney.com/quiz/2018/09/23/quiz-guess-90s-disney-movie-one-random-clue/"
+  },
+  {
+    headline: "Only a True Disney Princess Fan Can Get 100% on This Quiz",
+    image: "https://ohmy.disney.com/wp-content/uploads/2018/09/I_OhMyDisney_TruePrincessFansOnly_Featured-242x141.jpg",
+    url: "https://ohmy.disney.com/quiz/2018/09/22/true-disney-princess-fan-100-percent-quiz/"
+  },
+  {
+    headline: "Can You Guess the Disney Character Based on the Sketch We Drew with Our Non-Dominant Hand?",
+    image: "https://ohmy.disney.com/wp-content/uploads/2018/09/ET_OhMyDisney_NonDominantHandQuiz.jpg",
+    url: "https://ohmy.disney.com/quiz/2018/09/20/quiz-guess-character-sketch-non-dominant-hand/"
+  },
+  {
+    headline: "Match the Dwelling to the Disney Pixar Character That Lives There",
+    image: "https://ohmy.disney.com/wp-content/uploads/2018/09/et_ohmydisney_matchdwellingtocharacter.jpg",
+    url: "https://ohmy.disney.com/quiz/2018/09/17/quiz-match-dwelling-disney-pixar-character/"
+  }
+]
 
 function FooterPopular(){
   return (
     <div>
-      <p>FooterPopular</p>
+      {masterFooterPopularList.map((footerPopularPost, index) =>
+      <FooterPopularPost headline={footerPopularPost.headline}
+      image={footerPopularPost.image}
+      url={footerPopularPost.url}
+      key={index}/>
+      )}
     </div>
   );
 }
--- src/components/FooterPopularPost.jsx
+++ src/components/FooterPopularPost.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function FooterPopularPost() {
+function FooterPopularPost(props) {
   return (
     <div>
+      <a href={props.url}>
+        <div>
+          <div>
+            <img src={props.image}></img>
+          </div>
+          <div>
+            <h4>{props.headline}</h4>
+          </div>
+        </div>
+      </a>
     </div>
   );
+}
+
+FooterPopularPost.propTypes = {
+  headline: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 }
 
 export default FooterPopularPost;
