6695343920e822808047b18af4c2949b868c2f41
Nikki Wong <nikkiwong@miami.edu>
2018-11-09 13:41:55 -0800

    add retail post content

--- src/components/RetailPost.jsx
+++ src/components/RetailPost.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function RetailPost() {
+function RetailPost(props) {
   return (
     <div>
+      <img src={props.image}></img>
+      <div>
+        <div>
+          <p>$$<span>$</span></p>
+        </div>
+      </div>
+      <div>
+        <a href={props.url}>
+        <p>{props.shop}</p>
+        <h3>{props.headline}</h3>
+        </a>
+      </div>
     </div>
   );
 }
 
+RetailPost.propTypes = {
+  headline: PropTypes.string.isRequired,
+  shop: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  cost: PropTypes.number.isRequired,
+  url: PropTypes.string.isRequired
+}
+
 export default RetailPost;
+
+// Different Cost State for later
+// <div>
+//   <p class="cost">$<span class="gray">$$</span></p>
+// </div>
--- src/components/RetailPosts.jsx
+++ src/components/RetailPosts.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import RetailPost from './RetailPost';
+
+const masterRetailPostsList = [
+  {
+    headline: "OUR WISH IS TO WEAR THIS MAGIC CARPET DENIM JACKET EVERY SINGLE DAY",
+    shop: "DISNEY CLOTHES",
+    image: "https://ohmy.disney.com/wp-content/uploads/2018/10/C_OhMyDisney_AladdinJacket_02.jpg",
+    cost: 1, 
+    url: "https://ohmy.disney.com/shop/disney-clothes/oh-my-disney-collection-magic-carpet-denim-jacket-aladdin/"
+  },
+  {
+    headline: "THIS TOY STORY ALIEN ONESIE WILL MAKE YOU THE COZIEST PERSON THIS HALLOWEEN",
+    shop: "DISNEY CLOTHES",
+    image: "https://ohmy.disney.com/wp-content/uploads/2018/10/C_OhMyDisney_AlienOnesie_01.jpg",
+    cost: 1,
+    url: "https://ohmy.disney.com/shop/disney-clothes/shop-toy-story-alien-onesie/"
+  },
+  {
+    headline: "CELEBRATE MICKEY MOUSE’S UPCOMING ANNIVERSARY WITH THIS AMAZING HISTORY BOOK",
+    shop: "DISNEY BOOKS",
+    image: "https://ohmy.disney.com/wp-content/uploads/2018/10/C_OhMyDisney_MickeyMouseTaschenBook_01.jpg",
+    cost: 2, 
+    url: "https://ohmy.disney.com/shop/disney-book/shop-taschen-walt-disneys-mickey-mouse-ultimate-history/"
+  }
+]
 
 function RetailPosts(){
   return (
     <div>
-    <p>RetailPosts</p>
+    {masterRetailPostsList.map((retailPost, index) =>
+    <RetailPost headline={retailPost.headline}
+    shop={retailPost.shop}
+    image={retailPost.image}
+    cost={retailPost.cost}
+    url={retailPost.url}
+    key={index}/>
+    )}
     </div>
   );
 }
