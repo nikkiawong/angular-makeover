9f0096a33311c7331a5f160bf1c7d33eeffe829c
Nikki Wong <nikkiwong@miami.edu>
2018-11-09 15:01:08 -0800

    style popular sidebar post

--- src/components/PopularSidebarPost.jsx
+++ src/components/PopularSidebarPost.jsx
@@ -2,15 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function PopularSidebarPost(props) {
+  const popularSidebarPostStyles = {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    paddingTop: '20px',
+    paddingBottom: '10px',
+    borderBottom: '1px solid #d8d8d8'
+  }
+  const popularSidebarImageDivStyles = {
+    width: '50%',
+    paddingRight: '10px'
+  }
+  const popularSidebarImageStyles = {
+    width: '100%'
+  }
+  const popularSidebarContentStyles = {
+    width: '100%'
+  }
+  const popularSidebarHeadlineStyles = {
+    marginTop: '0',
+    marginLeft: '0',
+    fontWeight: '300'
+  }
+  const popularSidebarLinkStyles = {
+    color: 'black'
+  }
   return (
     <div>
-      <a href={props.url}>
-        <div>
-          <div>
-            <img src={props.image}></img>
+      <a style={popularSidebarLinkStyles} href={props.url}>
+        <div style={popularSidebarPostStyles}>
+          <div style={popularSidebarImageDivStyles}>
+            <img style={popularSidebarImageStyles} src={props.image}></img>
           </div>
-          <div>
-            <h4>{props.headline}</h4>
+          <div style={popularSidebarContentStyles}>
+            <h4 style={popularSidebarHeadlineStyles}>{props.headline}</h4>
           </div>
         </div>
       </a>
