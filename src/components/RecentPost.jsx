751224dacad40dfe0eeb8b395dbf31c6d37a7d03
Nikki Wong <nikkiwong@miami.edu>
2018-11-09 13:26:43 -0800

    style recent posts component

--- src/components/RecentPost.jsx
+++ src/components/RecentPost.jsx
@@ -2,16 +2,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function RecentPost(props) {
+  const recentPostStyles = {
+    display: 'flex',
+    flexDirection: 'row',
+    borderBottom: '1px solid #d8d8d8',
+    marginBottom: '20px',
+    paddingBottom: '20px',
+    textAlign: 'left',
+    alignItems: 'flex-start'
+  }
+  const recentPostHeadlineStyles = {
+    marginTop: '0'
+  }
+  const recentPostLinkStyles = {
+    color: '#dc2047'
+  }
+  const recentPostImageDivStyles = {
+    width: '70%',
+    marginRight: '20px'
+  }
+  const recentPostImageStyles = {
+    width: '100%'
+  }
+  const recentPostContentStyles = {
+    width: '100%'
+  }
   return (
     <div>
-      <a href={props.url}>
-        <div>
-          <div>
-            <img src={props.image}></img>
+      <a style={recentPostLinkStyles} href={props.url}>
+        <div style={recentPostStyles}>
+          <div style={recentPostImageDivStyles}>
+            <img style={recentPostImageStyles} src={props.image}></img>
           </div>
-          <div>
-            <h3>{props.headline}}</h3>
-            <p>{props.subheadline}}</p>
+          <div style={recentPostContentStyles}>
+            <h3 style={recentPostHeadlineStyles}>{props.headline}</h3>
+            <p>{props.subheadline}</p>
           </div>
         </div>
       </a>
