5031496777f277046907a8b615045004b506f911
Nikki Wong <nikkiwong@miami.edu>
2018-11-25 18:54:18 -0800

    add in all post page content

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -5,7 +5,6 @@ import CommentsControl from './CommentsControl';
 function PostPage() {
   const postPageBodyStyles = {
     width: '42%',
-    textAlign: 'center',
     marginLeft: 'auto',
     marginRight: 'auto',
     fontSize: '20px',
@@ -37,6 +36,9 @@ function PostPage() {
     display: 'inline',
     padding: '8px'
   }
+  const postPageImageStyles = {
+    width: '100%'
+  }
   return (
     <div>
       <div>
@@ -47,6 +49,8 @@ function PostPage() {
         </div>
         <div style={postPageBodyStyles}>
           <p>We are always excited to hear more about “Mickey: The True Original Exhibition” which is coming to New York City on November 8! Recently we learned about all the artists who will be participating in the 16,000-square-foot art exhibit celebrating Mickey Mouse’s 90th anniversary. Today, we have the information of all the rooms that will be on display for us to enjoy. Take a look at the list here:</p>
+          <img src='https://ohmy.disney.com/wp-content/uploads/2018/10/3900.jpg' style={postPageImageStyles}></img>
+          <p>“Mickey: The True Original Exhibition” runs from November 8, 2018 to February 10, 2019, and will be open Tuesday through Sunday from 10:00 a.m. to 8:00 p.m. The exhibition is located at 60 10th Avenue in New York City. Make sure to grab your tickets at Disney.com/MickeyTrueOriginal. We can’t wait to see you there!</p>
         </div>
         <Route exact path='/comments' component={CommentsControl}/>
       </div>
